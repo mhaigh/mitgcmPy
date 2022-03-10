5bf5b57aab4bb0413d2a0e0c6f7ac67bf71b36cb
Michael Haigh <michai@bas.ac.uk>
2022-03-10 17:25:01 +0000

    new functions in tools

--- tools.py
+++ tools.py
@@ -22,16 +22,16 @@ def maskEdges(data, size, dims=None):
 		
 #==
 	
-def getSubregionXY(data, grid, lons, lats):
+def getSubregionXY(data, lats, lons):
 	'''Given data with lat-lon dimensions in final two (or only two)
 	axes, reduce lateral range to range given by lats and lons.
 	Assumes lats and lons are tuples with two indices.'''
 		
-	return data[..., lons[0]:lons[1]+1, lats[0]:lats[1]+1]
+	return data[..., lats[0]:lats[1]+1, lons[0]:lons[1]+1]
 	
 #==
 
-def getSubregionXZ(data, grid, lons, depths, Ydim=False):
+def getSubregionXZ(data, lons, depths, Ydim=False):
 	'''Similar to above: return subregion of data in (y,z)-plane.
 	If data is 3- or 4-dimensional, there has to be y-dim between
 	z- and x-dims.
@@ -52,7 +52,7 @@ def getSubregionXZ(data, grid, lons, depths, Ydim=False):
 		
 #==
 	
-def getSubregionYZ(data, grid, lats, depths, Xdim=False):
+def getSubregionYZ(data, lats, depths, Xdim=False):
 	'''Similar to above: return subregion of data in (y,z)-plane.
 	By default this assumes that data does not have a Y-dimension.'''
 	
@@ -150,31 +150,34 @@ def depthIntegral(data, grid, timeDep=True, SSH=None, norm=True):
 		axis = 0
 
 	if norm:
-		norm = np.sum(grid.DRF * grid.hFacW, axis=0)
+		normval = np.sum(grid.DRF * grid.hFacW, axis=0)
 	else:
-		norm = 1
+		normval = 1
 
 	if SSH is not None:
 		DRF0 = grid.DRF.squeeze()[0] + SSH
+		if norm:
+			normval = normval + SSH
 		if timeDep:
-			T0 = data[:,0] * grid.hFacW[0]  * DRF0
-			return	(T0 + np.sum(data[:,1:] * grid.hFacW[1:] * grid.DRF[1:], axis=axis)) / norm
+			T0 = data[:,0] * grid.hFacW[0] * DRF0
+			return	(T0 + np.sum(data[:,1:] * grid.hFacW[1:] * grid.DRF[1:], axis=axis)) / normval
 		else: 
 			T0 = data[0] * grid.hFacW[0] * DRF0
-			return	(T0 + np.sum(data[1:] * grid.hFacW[1:] * grid.DRF[1:], axis=axis)) / norm
+			return	(T0 + np.sum(data[1:] * grid.hFacW[1:] * grid.DRF[1:], axis=axis)) / normval
 			
 	else:
-		return np.sum(data * grid.hFacW * grid.DRF, axis=axis) / norm
+		normval = np.where(normval==0.0, 1, normval)
+		return np.sum(data * grid.hFacW * grid.DRF, axis=axis) / normval
 
 #==
 
-def barotropicStreamfunction(u, grid, timeDep=True, SSH=None):
+def barotropicStreamfunction(u, grid, timeDep=True, SSH=None, norm=False):
 	'''Given zonal velocity, compute barotropic streamfunction.
 	If timeDep, assumes axis ordering is (t, z, y, x).
 	If not timeDep, assumes axis ordering is (z, y, x).'''
 	
 	# First, get depth-integrated mass transport.
-	T = depthIntegral(u, grid, timeDep=timeDep, SSH=SSH)
+	T = depthIntegral(u, grid, timeDep=timeDep, SSH=SSH, norm=norm)
 
 	# Second, integrate from southern boundary to northern boundary.
 	if timeDep:
@@ -192,11 +195,10 @@ def barotropicStreamfunction(u, grid, timeDep=True, SSH=None):
 	#	for yi in range(0,Y.shape[0]):
 	#		strfunc[:,yi] = - np.trapz(T[:,:yi+1,], Y[:yi+1,0], axis=axis)
 
-	# Simple option using cumulative sum.
-	if timeDep:
-		strfunc = - np.cumsum(T * grid.DYG, axis=1)
-	
-	return strfunc
+	# Simple option using cumulative sum. Convert to SV.
+	return - 1.e-6 * np.cumsum(T * grid.DYG, axis=axis)
+
+#==
 
 def meridStreamfunction(v, X, Z, timeDep=True):
 	'''Given meridional velocity, return meridional overturning streamfunction.
@@ -288,7 +290,10 @@ def bottom(data, grid, cellPos, timeDep=True):
 		print('Error: tools.bottom. Must provide valid cellPos.')
 		sys.exit()
 
-	Nt, Nz, Ny, Nx = data.shape
+	if timeDep:
+		Nt, Nz, Ny, Nx = data.shape
+	else:
+		Nz, Ny, Nx = data.shape
 
 	# Get depth index of ocean floor.
 	bottom = np.zeros((Ny, Nx))
@@ -303,7 +308,10 @@ def bottom(data, grid, cellPos, timeDep=True):
 	X = [[i]*Ny for i in range(Nx)]
 	X = [X[j][i] for i in range(Ny) for j in range(Nx)]
 
-	datab = data[:,bottom.flatten(),Y,X].reshape((Nt,Ny,Nx))
+	if timeDep:
+		datab = data[:,bottom.flatten(),Y,X].reshape((Nt,Ny,Nx))
+	else:
+		datab = data[bottom.flatten(),Y,X].reshape((Ny,Nx))
 
 	# Commmented out a brute-force approach to above list comprehension solution.
 	#datab = np.zeros((Nt,Ny,Nx))
@@ -325,8 +333,29 @@ def EOSlinear(rho0, sBeta, tAlpha, S, Sref, T, Tref):
 	return rho0 * (sBeta * (S - Sref) - tAlpha * (T - Tref)) 
 
 	
-	
-	
+#==
+
+def interp(data, edge):
+	'''Interpolate data from cell edges to cell centres.
+	Interpolate from either u points or v points.'''
+
+	data_interp = np.zeros(data.shape)
+
+	if edge == 'u':
+		print('Interpolating from u points. Averaging between masked/unmasked neighbours OK if no-slip/no-normal flow, provided masked areas set to zero. Assuming periodic in x.' )
+		data_interp[...,:-1] = 0.5 * (data[...,:-1] + data[...,1:])
+		data_interp[...,-1] = 0.5 * (data[...,-1] + data[...,0])
+
+	elif edge == 'v':
+		print('Interpolating from v points. Averaging between masked/unmasked neighbours OK if no-slip, provided masked areas set to zero. Assuming periodic in y.' )
+		data_interp[...,:-1,:] = 0.5 * (data[...,:-1,:] + data[...,1:,:])
+		data_interp[...,-1,:] = 0.5 * (data[...,-1,:] + data[...,0,:])
+
+	else:
+		print('Error: tools.interp. Must provide valid edge value.')
+		sys.exit()
+
+	return data_interp
 	
 	
 
