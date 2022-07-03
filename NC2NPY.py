18c4dd4837e534d6f2000e294f08d45f511e0482
Michael Haigh <m.haigh3@hotmail.com>
2022-07-03 16:08:10 +0100

    NC2NPY

---
+++ NC2NPY.py
@@ -0,0 +1,41 @@
+import sys
+
+import numpy as np
+
+from grid import Grid
+from readData import readVariable
+
+#==========================================================
+
+EXP = sys.argv[1]
+VAR = sys.argv[2]
+
+#path = '/data/oceans_output/shelf/michai/mitgcm/MCS_125/run/'
+path = '/home/michael/Documents/data/'+EXP+'/run/'
+
+depths = [-10,-450]
+levels = []
+
+ts = 0
+
+#==
+
+grid = Grid(path)
+for depth in depths:
+	levels.append(grid.getIndexFromDepth(depth))
+		
+data = readVariable(VAR, path, meta=False)[ts:,levels,]
+data = np.ma.filled(data, fill_value=0)
+
+outname = VAR + '_' + EXP + '_lvls'
+for level in levels:
+	outname += '_' + str(level)
+	
+np.save(outname, data)
+	
+#data1 = readVariable(VAR, path, meta=False)[ts:,]
+#import plotting as pt
+#pt.plot1by2([data[-1,0], data1[-1,levels[0]]])
+#pt.plot1by2([data[-1,1], data1[-1,levels[1]]])
+
+
