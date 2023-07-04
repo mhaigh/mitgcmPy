ac137e25be82b58b02024c46cb84f21024e654ba
mhaigh <m.haigh3@hotmail.com>
2023-07-04 20:11:41 +0100

    Update PAS.py

--- PAS.py
+++ PAS.py
@@ -2,10 +2,6 @@ import numpy as np
 
 
 
-from time import ctime
-
-
-
 from grid_PAS import Grid as Grid_PAS
 
 
@@ -15,6 +11,10 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as cl
 
 import matplotlib.animation as animation
+
+
+
+from time import ctime
 
 
 
@@ -38,7 +38,9 @@ from readData import *
 
 
 
-DATADIR = '/home/michael/Documents/data/'
+DATADIR = '/data/oceans_output/shelf/pahol/mitgcm/'
+
+#DATADIR = '/home/michael/Documents/data/'
 
 PASDIR = DATADIR + 'PAS_851/run/'
 
@@ -48,29 +50,7 @@ EASlats = [-75.5, -68]
 
 
 
-westGetz = [1, 60]
-
-Getz = [60, 65]
-
-westPITW = [65, 105]
-
-PITW = [105, 120]
-
-westPITE = [120, 180]
-
-PITE = [180, 230]
-
-eastPITE = [230, 250]
-
-	
-
-sections = {'westGetz':westGetz, 'Getz':Getz, 'westPITW':westPITW, 'PITW':PITW, 'westPITE':westPITE, 'PITE':PITE, 'eastPITE':eastPITE}
-
-	
-
-
-
-MAIN = False
+MAIN = True
 
 if MAIN:
 
@@ -116,13 +96,13 @@ if MAIN:
 
 	slope_x, slope_y = PAS_tools.getSlopeContour(bathy, X, Y, pathno)
 
-	bathy = ptt.maskBathyXY(bathy, grid, zi=0, subregion=True, lats=latsi, lons=lonsi)
-
-	
-
 	np.save('slope_x', slope_x); np.save('slope_y', slope_y)
 
-	
+
+
+	bathy = ptt.maskBathyXY(bathy, grid, zi=0, subregion=True, lats=latsi, lons=lonsi)
+
+
 
 	# Get bearing of slope (using a centred differencing, one-sided at ends of slope).
 
@@ -162,8 +142,6 @@ if MAIN:
 
 
 
-	np.save('slope_xi', slope_xi); np.save('slope_yi', slope_yi)
-
 	# Plot bathy, check contour points and bearing.
 
 	#plt.subplot(121); plt.contourf(X, Y, bathy); plt.scatter(slope_x_nX, slope_y_nX)
@@ -182,31 +160,49 @@ if MAIN:
 
 	
 
-	slice_lon = 252.5
-
-	xi = grid.getIndexFromLon(slice_lon)
-
 	zs = [0, -1000]
 
 	zz = grid.getIndexFromDepth(zs)
 
 
 
-	nT = 12
-
-	slope_uv_av = np.zeros((nT, nX))
-
-	slope_uv_max = np.zeros((nT, nX))
-
 	ufile = 'UVEL'; vfile = 'VVEL';	rhofile = 'RHOAnoma'
 
-	for ti in range(1):
+	uwindfile = 'EXFuwind'; vwindfile = 'EXFvwind'
 
-		for xi in range(50,nX):
+	ustressfile = 'oceTAUX'; vstressfile = 'oceTAUY'	
+
+
+
+	#t_start = 0; t_end = 10
+
+	#t_start = 107; t_end = 622
+
+	t_start = 0; t_end = 779
+
+	surf_uv_max = np.zeros((t_end-t_start, nX))
+
+	surf_uv_av = np.zeros((t_end-t_start, nX))
+
+	slope_uv_av = np.zeros((t_end-t_start, nX))
+
+	slope_uv_max = np.zeros((t_end-t_start, nX))	
+
+	slope_uw_av = np.zeros((t_end-t_start, nX))
+
+	slope_us_av = np.zeros((t_end-t_start, nX))
+
+	for ti in range(t_start, t_end):
+
+		print(ti)
+
+		print(ctime(readVariable(rhofile, PASDIR, file_format='nc', meta=True)['TIME'][ti]))
+
+		for xi in range(nX):
 
 			
 
-			yy = [yshift+slope_yi[xi]-3, yshift+slope_yi[xi]+3]
+			yy = [yshift+slope_yi[xi]-3, yshift+slope_yi[xi]+1]
 
 			rho = 1028.5 + readVariable(rhofile, PASDIR, file_format='nc', meta=False, tt=ti, xx=xi+xshift, yy=yy, zz=zz)
 
@@ -216,6 +212,8 @@ if MAIN:
 
 			
 
+			# Load velocities
+
 			u = readVariable(ufile, PASDIR, file_format='nc', meta=False, tt=ti, xx=xi+xshift, yy=yy, zz=zz)
 
 			u = ptt.maskBathyYZ(u, grid, xi=xi+xshift, subregion=True, lats=yy, depths=zz, sameIasNC=True)
@@ -224,35 +222,65 @@ if MAIN:
 
 			v = ptt.maskBathyYZ(v, grid, xi=xi+xshift, subregion=True, lats=yy, depths=zz, sameIasNC=True)
 
-			
+		
 
-			u = tools.interp(u, 'u'); v = tools.interp(v, 'v')
+			# Load wind and stress
 
-			
+			uw = readVariable(uwindfile, PASDIR, file_format='nc', meta=False, tt=ti, xx=xi+xshift, yy=yy, var2D=True)
+
+			vw = readVariable(vwindfile, PASDIR, file_format='nc', meta=False, tt=ti, xx=xi+xshift, yy=yy, var2D=True)
+
+			us = readVariable(ustressfile, PASDIR, file_format='nc', meta=False, tt=ti, xx=xi+xshift, yy=yy, var2D=True)
+
+			vs = readVariable(vstressfile, PASDIR, file_format='nc', meta=False, tt=ti, xx=xi+xshift, yy=yy, var2D=True)
+
+
+
+			#==
+
+		
+
+			# Get along-slope surface and undercurrent flow speeds. 
+
+		
 
 			slope_uv = u * np.sin(bearing_nX[xi]*np.pi/180.) + v * np.cos(bearing_nX[xi]*np.pi/180.)
 
-
-
 			# Mask along-slope velocity before computing average.
+
+			surf_uv_av[ti-t_start,xi] = np.ma.mean(slope_uv[0])
+
+			surf_uv_max[ti-t_start,xi] = np.ma.max(slope_uv[0])
 
 			slope_uv = np.ma.masked_where(rho<1028., slope_uv)
 
-			#ZZZ = np.tile(grid.RC.squeeze()[zz[0]:zz[1]], (slope_uv.shape[1],1)).T
+			ZZZ = np.tile(grid.RC.squeeze()[zz[0]:zz[1]], (slope_uv.shape[1],1)).T
 
-			#slope_uv = np.ma.masked_where(ZZZ<-800, slope_uv)
+			slope_uv = np.ma.masked_where(ZZZ<-800, slope_uv)
 
-			slope_uv_av[ti,xi] = np.ma.mean(slope_uv)
+	
 
-			slope_uv_max[ti,xi] = np.ma.max(slope_uv)
+			slope_uv_av[ti-t_start,xi] = np.ma.mean(slope_uv)
+
+			slope_uv_max[ti-t_start,xi] = np.ma.max(slope_uv)		
 
 			#lim = 0.09; slope_uv = tools.boundData(slope_uv, -lim, lim)
 
 			#slope_uv[0,0] = -lim; slope_uv[0,1] = lim
 
-			pt.plot1by2([bathy, slope_uv], contour=[None, rho], X=[X,Y[yy[0]:yy[1],0]], Y=[Y,Z[zz[0]:zz[1]]], vlines=[[X[0,xi]],None])
+			#pt.plot1by2([bathy, slope_uv], contour=[None, rho], X=[X,Y[yy[0]:yy[1],0]], Y=[Y,Z[zz[0]:zz[1]]], vlines=[[X[0,xi]],None])
 
-			
+			# Get along-slope wind and wind stress.
+
+			slope_uw = uw * np.sin(bearing_nX[xi]*np.pi/180.) + vw * np.cos(bearing_nX[xi]*np.pi/180.)
+
+			slope_us = us * np.sin(bearing_nX[xi]*np.pi/180.) + vs * np.cos(bearing_nX[xi]*np.pi/180.)
+
+			slope_uw_av[ti-t_start,xi] = np.ma.mean(slope_uw)
+
+			slope_us_av[ti-t_start,xi] = np.ma.mean(slope_us)
+
+
 
 			#==
 
@@ -268,19 +296,27 @@ if MAIN:
 
 	
 
+	np.save('surf_uv_max', surf_uv_max)
+
+	np.save('surf_uv_av', surf_uv_av)
+
 	np.save('slope_uv_av', slope_uv_av)
 
 	np.save('slope_uv_max', slope_uv_max)
 
-		
+	np.save('slope_uw_av', slope_uw_av)
 
-	for ti in [1,3,5,7,9,11]:#range(nT):
+	np.save('slope_us_av', slope_us_av)
 
-		plt.plot(slope_uv_av[ti], label=ti)
+	
 
-	plt.legend()
+	#for ti in [1,3,5,7,9,11]:#range(nT):
 
-	plt.show()
+	#	plt.plot(slope_uv_av[ti], label=ti)
+
+	#plt.legend()
+
+	#plt.show()
 
 			
 
@@ -291,404 +327,6 @@ if MAIN:
 	# Read u, v at this point and n grid points north and south.
 
 	# Get component of each in along-slope direction
-
-
-
-#==
-
-
-
-readSlopeUVfile = True
-
-if readSlopeUVfile:
-
-
-
-	#t_start = 107; t_end = 622
-
-	t_start = 0; t_end = 779
-
-	
-
-	nl2 = int(5*12); nl = 2*nl2 + 1
-
-	lags = np.linspace(-nl2,nl2,nl)
-
-	
-
-	path = '/home/michael/Documents/Python/mitgcmPy/movies/31_05_23/'
-
-	path = path + str(t_start) + '_' + str(t_end) + '_y1/'
-
-	
-
-	uvfile = 'slope_uv_max.npy'
-
-	surfuvfile = 'surf_uv_max.npy'
-
-	timefile = 'PAS_time.npy'
-
-	slopexfile = 'slope_x.npy'
-
-	slopeyfile = 'slope_y.npy'
-
-	
-
-	slope_uv = np.load(path+uvfile)
-
-	surf_uv = np.load(path+surfuvfile)
-
-	time = np.load(path+timefile)
-
-	slope_x = np.load(path+slopexfile)
-
-	slope_y = np.load(path+slopeyfile)
-
-	
-
-	#plt.scatter(slope_x, slope_y); plt.show(); quit()
-
-
-
-	nn = 60
-
-	
-
-	#===
-
-	months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-	monthDec = {}
-
-	for mi in range(len(months)):
-
-		monthDec[months[mi]] = mi/12.
-
-	year = [float(ctime(int(time_))[-4:])-15+monthDec[ctime(int(time_))[4:7]] for time_ in time[t_start:t_end]]#
-
-	
-
-	start = 24*12 + 5*12
-
-	year = year[start:] 
-
-
-
-	seasonal_cycle = 0
-
-	if seasonal_cycle:
-
-		slope_uv = slope_uv[start:] - slope_uv[start-12:-12]
-
-		surf_uv = surf_uv[start:] - surf_uv[start-12:-12]
-
-	else:
-
-		slope_uv = slope_uv[start:]
-
-		surf_uv = surf_uv[start:]
-
-		
-
-	year_ = year[nn//2:-nn//2+1]
-
-	
-
-	# Silvano years: 1987 - 2017 (+-2.5)
-
-	ysSil = np.argmin(np.abs(np.array(year)-1987.+2.5)); yeSil = np.argmin(np.abs(np.array(year)-2017.-2.5))
-
-	ys_Sil = np.argmin(np.abs(np.array(year_)-1987.+2.5)); ye_Sil = np.argmin(np.abs(np.array(year_)-2017.-2.5))
-
-	
-
-	ys = ysSil; ye = yeSil
-
-	ys_ = ys_Sil; ye_ = ye_Sil
-
-	
-
-	plotSections = ['westGetz', 'westPITW', 'westPITE']
-
-
-
-	# Plot average evaluated over all sections.
-
-	if 1:
-
-		uv_sum = np.zeros(len(year))
-
-		surf_uv_sum = np.zeros(len(year))
-
-		nx = 0
-
-		for section in plotSections:
-
-			iw = sections[section][0]; ie = sections[section][1]
-
-			uv_sum += np.sum(slope_uv[:,iw:ie], axis=1)
-
-			surf_uv_sum += np.sum(surf_uv[:,iw:ie], axis=1)
-
-			nx += ie-iw
-
-		
-
-		uv_mean = uv_sum / nx
-
-		surf_uv_mean = surf_uv_sum / nx
-
-		
-
-		#uv_mean = uv_mean[ys:ye]; surf_uv_mean = surf_uv_mean[ys:ye]; year = year[ys:ye]	
-
-		uv_mean = PAS_tools.demean(uv_mean)
-
-		surf_uv_mean = PAS_tools.demean(surf_uv_mean)
-
-		
-
-		corruv = PAS_tools.crossCorr(uv_mean, surf_uv_mean, nl, nl2)
-
-		corruu = PAS_tools.crossCorr(uv_mean, uv_mean, nl, nl2)
-
-		corrvv = PAS_tools.crossCorr(surf_uv_mean, surf_uv_mean, nl, nl2)
-
-		Fcuv, freq, period = PAS_tools.fft(corruv)
-
-		Fcuu, freq, period = PAS_tools.fft(corruu)
-
-		Fcvv, freq, period = PAS_tools.fft(corrvv)
-
-		
-
-		Puv = PAS_tools.fftPower(Fcuv, nl2)
-
-		Puu = PAS_tools.fftPower(Fcuu, nl2)
-
-		Pvv = PAS_tools.fftPower(Fcvv, nl2)
-
-		coh = Puv**2 / (Puu * Pvv)
-
-		
-
-		# Get correlations after a range of running means applied to data.
-
-		nw = 60
-
-		window_lengths = np.linspace(1,nw,nw)
-
-		corrw, tw, uw, vw = PAS_tools.windowCorr(uv_mean, surf_uv_mean, window_lengths, year, return_uv=True)
-
-		
-
-		# Plot correlations for velocities averaged with range of running mean windows.
-
-		wis = [0,12,18,48]
-
-		plt.figure(figsize=(16,17))
-
-		for wi in range(len(wis)):
-
-			plt.subplot(2,2,wi+1)
-
-			plt.plot(tw[wis[wi]], uw[wis[wi]], color='r', label='deep vel')
-
-			plt.plot(tw[wis[wi]], vw[wis[wi]], color='k', label='surf vel')
-
-			plt.xlabel('time (years)'); plt.grid()
-
-			title = str(wis[wi]/12) + '-year running mean, corr = {:.2f}'.format(corrw[wis[wi]])
-
-			plt.title('u, v time series, ' + title)
-
-			plt.legend()
-
-		plt.show()
-
-		
-
-		plt.figure(figsize=(13,6))
-
-		plt.subplot(121); plt.plot(lags/12., corruv)
-
-		plt.grid(); plt.title('corr(surface current, undercurrent)'); plt.xlabel('lag (years)')
-
-		plt.subplot(122); plt.plot(period, np.real(Fcuv), label='Real'); plt.plot(period, np.imag(Fcuv), label='Imag')
-
-		plt.plot(period, np.abs(Fcuv), label='abs'); plt.gca().set_xscale('symlog', base=2)
-
-		plt.grid(); plt.xlabel('Period (months)'); plt.legend(); plt.title('corr Fourier spectrum');
-
-		#plt.xticks([2**n for n in range(len(Fcuv))])
-
-		plt.show()
-
-		
-
-		plt.plot(period[nl2:]/12,Puv)
-
-		plt.gca().set_xscale('log')
-
-		plt.show()	
-
-			
-
-		#== 
-
-		
-
-		uv_mean = PAS_tools.windowAv(uv_mean, n=nn)[nn//2:-nn//2+1]
-
-		surf_uv_mean = PAS_tools.windowAv(surf_uv_mean, n=nn)[nn//2:-nn//2+1]
-
-		year = PAS_tools.windowAv(year, n=nn)[nn//2:-nn//2+1]
-
-
-
-		plt.plot(year, uv_mean, label='Deep along-slope flow', color='r')
-
-		plt.plot(year, surf_uv_mean, label='Surface along-slope flow', color='k')		
-
-		#plt.plot(year_[ys_:ye_], uv_mean[ys_:ye_], label='Deep along-slope flow')
-
-		#plt.plot(year_[ys_:ye_], surf_uv_mean[ys_:ye_], label='Surface along-slope flow')
-
-		plt.title('Along-slope average current speed (m/s)')
-
-		#plt.ylim(0, 0.15)
-
-		plt.grid()
-
-		plt.legend()
-
-		plt.show()
-
-		
-
-	# Plot average for each section.
-
-	if 0:
-
-		for section in plotSections:
-
-			iw = sections[section][0]; ie = sections[section][1]
-
-			uv = np.mean(slope_uv[:,iw:ie], axis=1)
-
-			uv = window_average(uv, n=nn)[nn//2:-nn//2+1]
-
-			plt.plot(year_, uv, label=section)
-
-		plt.title('Along-slope average current speed (m/s)')
-
-		#plt.ylim(0, 0.15)
-
-		plt.grid()
-
-		plt.legend()
-
-		plt.show()
-
-	
-
-	quit()
-
-	
-
-	nt = 20*12
-
-
-
-	
-
-	t = np.linspace(1,nt,nt)
-
-	lags = np.linspace(-nl2,nl2,nl)
-
-	
-
-	rand1 = np.random.random(nt); rand2 = np.random.random(nt); rand3 = np.random.random(nt); rand4 = np.random.random(nt)
-
-	rand1 = 1; rand2 = 1; rand3 = 1; rand4 = 1
-
-	p1 = 4*12; p2 = 4*12; p3 = 1.5*12
-
-	u = 1*np.sin(2*np.pi*t/p1) + 0*rand1*np.cos(2*np.pi*t/(rand2*p3))
-
-	v = -0*np.sin(2*np.pi*t/p1) - 1*rand3*np.cos(2*np.pi*t/(rand4*p3))
-
-	
-
-	#plt.plot(u); plt.plot(v); plt.show()
-
-	
-
-	#==
-
-	
-
-	# Get correlations
-
-	
-
-	# Compute coherence.
-
-	# 1. Get auto and cross correlations as functions of lag.
-
-	# 2. Fourier transform correlations.
-
-
-
-	corruv = PAS_tools.crossCorr(u, v, nl, nl2)
-
-	corruu = PAS_tools.crossCorr(u, u, nl, nl2)
-
-	corrvv = PAS_tools.crossCorr(v, v, nl, nl2)
-
-	corrvu = PAS_tools.crossCorr(v, u, nl, nl2)
-
-		
-
-	#plt.plot(lags, corruv); plt.plot(lags, corrvu); plt.grid(); plt.show(); quit()
-
-	
-
-	#==
-
-	
-
-	# Get correlations after a range of running means applied to data.
-
-	nw = 60
-
-	window_lengths = np.linspace(1,nw,nw)
-
-	corrw, tw, uw, vw = PAS_tools.windowCorr(u, v, window_lengths, t, return_uv=True)
-
-#==
-
-
-
-saveTime = False
-
-if saveTime:
-
-
-
-	rhofile = 'RHOAnoma'
-
-	rho = readVariable(rhofile, PASDIR, file_format='nc', meta=True)
-
-	time = np.array(rho['TIME'][:])
-
-	np.save('PAS_time', time)
-
-	print(time.shape)
-
-	print(time)
-
-	quit()
 
 
 
@@ -726,13 +364,15 @@ if animateX:
 
 		X, Y = grid.XYsubr(EASlons, EASlats)
 
-		pathno = 0
+		pathno = 1
 
 		xshift = lonsi[0]
 
 		yshift = latsi[0]
 
 	nY, nX = X.shape
+
+
 
 
 
@@ -800,6 +440,10 @@ if animateX:
 
 	
 
+	slice_lon = 252.5
+
+	xi = grid.getIndexFromLon(slice_lon)
+
 	zs = [0, -1000]
 
 	zz = grid.getIndexFromDepth(zs)
@@ -815,8 +459,6 @@ if animateX:
 		
 
 		for xi in range(nX):
-
-			print(xi)
 
 			
 
@@ -840,23 +482,17 @@ if animateX:
 
 			v = ptt.maskBathyYZ(v, grid, xi=xi+xshift, subregion=True, lats=yy, depths=zz, sameIasNC=True)
 
-			print(yy)
+			
 
 			slope_uv = u * np.sin(bearing_nX[xi]*np.pi/180.) + v * np.cos(bearing_nX[xi]*np.pi/180.)
 
-			
-
-			bathy = tools.boundData(bathy, -4.e3, -4.e2)
-
-			lim = 0.06;	slope_uv = tools.boundData(slope_uv, -lim, lim)
+			lim = 0.09;	slope_uv = tools.boundData(slope_uv, -lim, lim)
 
 			slope_uv[0,0] = -lim; slope_uv[0,1] = lim
 
 			
 
-			title0 = 'Bathymetry (m), xi='+str(xi) + ', ' + f'bearing = {bearing_nX[xi]:.1f}'
-
-			pt.plot1by2([bathy, slope_uv], contour=[None, rho], X=[X,Y[yy[0]-yshift:yy[1]-yshift,0]], Y=[Y,Z[zz[0]:zz[1]]], vlines=[[X[0,xi]],None], titles=[title0, 'Along-slope flow speed (m/s)'], show=False, save=True, outname=imgpath+f"{xi:03}", cmaps=['viridis', 'coolwarm'])
+			pt.plot1by2([bathy, slope_uv], contour=[None, rho], X=[X,Y[yy[0]:yy[1],0]], Y=[Y,Z[zz[0]:zz[1]]], vlines=[[X[0,xi]],None], titles=['Bathymetry (m)', 'Along-slope flow speed (m/s)'], show=False, save=True, outname=imgpath+f"{xi:03}")
 
 			
 
@@ -919,6 +555,32 @@ if animateX:
 	
 
 	
+
+#==
+
+
+
+saveTime = True
+
+if saveTime:
+
+
+
+	rhofile = 'RHOAnoma'
+
+	rho = readVariable(rhofile, PASDIR, file_format='nc', meta=True)
+
+	time = np.array(rho['TIME'][:])
+
+	np.save('PAS_time', time)
+
+	print(time.shape)
+
+	print(time)
+
+	quit()
+
+
 
 #==
 
@@ -1046,90 +708,6 @@ if latLonToCartesian1:
 
 
 
-plotSections = False
-
-if plotSections:
-
-
-
-	t_start = 107; t_end = 502	
-
-	path = '/home/michael/Documents/Python/mitgcmPy/movies/31_05_23/'
-
-	path = path + str(t_start) + '_' + str(t_end) + '/'
-
-	
-
-	slopexfile = 'slope_x.npy';	slopeyfile = 'slope_y.npy'
-
-	slopexifile = 'slope_xi.npy';	slopeyifile = 'slope_yi.npy'
-
-	slope_x = np.load(path+slopexfile);	slope_y = np.load(path+slopeyfile)
-
-	slope_xi = np.load(path+slopexifile); slope_yi = np.load(path+slopeyifile)
-
-	
-
-	grid = Grid_PAS(PASDIR)
-
-	X = grid.XC; Y = grid.YC
-
-	bathy = grid.bathy
-
-	bathy = ptt.maskBathyXY(bathy, grid, zi=0)
-
-	bathy = tools.boundData(bathy, -4000, -400)
-
-	
-
-	#plt.scatter(slope_xi, slope_yi); plt.show(); 
-
-	
-
-	subr = 1
-
-	if subr:
-
-		# Get subregion of bathymetry.
-
-		latsi = grid.getIndexFromLat(EASlats); lonsi = grid.getIndexFromLon(EASlons)
-
-		bathy = tools.getSubregionXY(bathy, latsi, lonsi)
-
-		X, Y = grid.XYsubr(EASlons, EASlats)
-
-		bathy = ptt.maskBathyXY(bathy, grid, zi=0, subregion=True, lats=latsi, lons=lonsi)
-
-		xshift = lonsi[0]; yshift = latsi[0]
-
-
-
-	plotSections = ['westGetz', 'westPITW', 'westPITE']
-
-	plt.contourf(X, Y, bathy)
-
-	for sec in plotSections:
-
-		iw = sections[sec][0]; ie = sections[sec][1]
-
-		plt.plot([X[0,iw], X[0,iw]], [Y[slope_yi[iw]-10,0], Y[slope_yi[iw]+10,0]], linestyle='dashed', color='k')
-
-		plt.plot([X[0,ie], X[0,ie]], [Y[slope_yi[ie]-10,0], Y[slope_yi[ie]+10,0]], linestyle='dashed', color='k')
-
-	plt.colorbar()
-
-	plt.tight_layout()
-
-	plt.show()
-
-	
-
-	
-
-#==
-
-
-
 latLonToCartesian2 = False
 
 if latLonToCartesian2:
@@ -1153,168 +731,6 @@ if latLonToCartesian2:
 	pt.plot1by2([x, y])
 
 	
-
-#==
-
-
-
-coherence = False
-
-if coherence:
-
-
-
-	nt = 20*12
-
-	nl2 = 10*12
-
-	nl = 2*nl2 + 1
-
-	
-
-	t = np.linspace(1,nt,nt)
-
-	lags = np.linspace(-nl2,nl2,nl)
-
-	
-
-	rand1 = np.random.random(nt); rand2 = np.random.random(nt); rand3 = np.random.random(nt); rand4 = np.random.random(nt)
-
-	rand1 = 1; rand2 = 1; rand3 = 1; rand4 = 1
-
-	p1 = 4*12; p2 = 4*12; p3 = 2.6
-
-	u = .5*np.sin(2*np.pi*t/p1) + 0*rand1*np.cos(2*np.pi*t/(rand2*p3))
-
-	v = -.5*np.cos(2*np.pi*t/p2) + 0*rand3*np.cos(2*np.pi*t/(rand4*p3))
-
-	
-
-	#plt.plot(u); plt.plot(v); plt.show()
-
-
-
-	#==
-
-	
-
-	# Get correlations
-
-	
-
-	# Compute coherence.
-
-	# 1. Get auto and cross correlations as functions of lag.
-
-	# 2. Fourier transform correlations.
-
-
-
-	corruv = PAS_tools.crossCorr(u, v, nl, nl2)
-
-	corruu = PAS_tools.crossCorr(u, u, nl, nl2)
-
-	corrvv = PAS_tools.crossCorr(v, v, nl, nl2)
-
-	corrvu = PAS_tools.crossCorr(v, u, nl, nl2)
-
-	#coh = PAS_tools.coherence(corruv, corruu, corrvv)
-
-	
-
-	#plt.plot(lags, corruv); plt.plot(lags, corrvu); plt.grid(); plt.show(); quit()
-
-	
-
-	#==
-
-	
-
-	# Get correlations after a range of running means applied to data.
-
-	nw = 60
-
-	window_lengths = np.linspace(1,nw,nw)
-
-	corrw, tw, uw, vw = PAS_tools.windowCorr(u, v, window_lengths, t, return_uv=True)
-
-	#==
-
-	
-
-	wis = [0,12,18,48]
-
-	plt.figure(figsize=(16,17))
-
-	for wi in range(len(wis)):
-
-		plt.subplot(2,2,wi+1)
-
-		plt.plot(tw[wis[wi]], uw[wis[wi]])
-
-		plt.plot(tw[wis[wi]], vw[wis[wi]])
-
-		plt.xlabel('time (years)'); plt.grid()
-
-		title = str(wis[wi]/12) + '-year running mean, corr = {:.2f}'.format(corrw[wis[wi]])
-
-		plt.title('u, v time series, ' + title)
-
-	plt.show()
-
-
-
-	#Fv = np.fft.fftshift(np.fft.fft(u)); freqt = 1./np.fft.fftshift(np.fft.fftfreq(nt))
-
-	#plt.plot(freqt, np.real(Fv), label='Real'); plt.plot(freqt, np.imag(Fv), label='Imag'); plt.legend(); plt.show(); quit()
-
-	
-
-	Fcorruv = np.fft.fftshift(np.fft.fft(corruv))
-
-	freq = np.fft.fftshift(np.fft.fftfreq(nl))
-
-	
-
-	plt.figure(figsize=(16,17))
-
-	plt.subplot(221)
-
-	plt.plot(t[:]/12, u[:], label='u = sin'+str(p1/12) + '+ cos'+str(p3/12))
-
-	plt.plot(t[:]/12, v[:], label='v = -sin'+str(p1/12) + '+ cos'+str(p3/12))
-
-	plt.xlabel('time (years)'); plt.title('u, v time series'); plt.legend(); plt.grid()
-
-	plt.subplot(222)
-
-	plt.plot((window_lengths-1)/12, corrw);  plt.xlabel('running mean window size (years)')
-
-	plt.title('corr(u,v) vs running mean window size'); plt.grid()
-
-	
-
-	plt.subplot(223)
-
-	plt.plot(lags/12., corruv); plt.grid(); plt.xlabel('Lag (years)'); plt.title('corr(u,v) vs lag');
-
-	plt.subplot(224)
-
-	plt.plot(1./12./freq, np.real(Fcorruv), label='Real power')
-
-	plt.plot(1./12./freq, np.imag(Fcorruv), label='Imag power'); plt.legend()
-
-	plt.xlabel('Period (years)'); plt.title('corr(u,v) Fourier power spectrum'); plt.grid();
-
-
-
-	plt.show()
-
-	
-
-	
-
-	quit()
 
 	
 
